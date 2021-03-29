a6fe0fe71cf02ceb864788542e53b11e54022215
Jordilob <jordi.bravo.bad@gmail.com>
2021-03-29 20:02:09 -0300

    Log in box

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -17,8 +17,125 @@ class LogInPage extends StatelessWidget {
         body: Stack(
           children: [
             _createBG(context),
+            _loginForm(context),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final TextStyle style = TextStyle(
+      fontSize: 20,
+    );
+
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              height: 180,
+            ),
+          ),
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric(vertical: 30.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.4),
+                  offset: Offset(0.0, 3.0),
+                  blurRadius: 3.0,
+                  spreadRadius: 3.0,
+                ),
+              ],
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Log In',
+                  style: style,
+                ),
+                _createEmail(),
+                _createPassword(),
+                _createButton(),
+              ],
+            ),
+          ),
+          SizedBox(height: 30.0),
+          Text('Forgot password?',
+              style: TextStyle(fontWeight: FontWeight.w300)),
+          SizedBox(height: 100.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _createEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: TextField(
+        cursorColor: Colors.deepPurple,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          labelStyle: TextStyle(color: Colors.black26),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+          ),
+          hintText: 'example@mail.com',
+          labelText: 'E-Mail',
+        ),
+      ),
+    );
+  }
+
+  Widget _createPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0),
+      child: TextField(
+        cursorColor: Colors.deepPurple,
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.vpn_key_outlined, color: Colors.deepPurple),
+          labelStyle: TextStyle(color: Colors.black26),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+          ),
+          hoverColor: Colors.deepPurple,
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+
+  Widget _createButton() {
+    final TextStyle style = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Padding(
+      padding: EdgeInsets.only(top: 30.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          ),
+          elevation: MaterialStateProperty.all(0.0),
+          textStyle: MaterialStateProperty.all(style),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 15.0),
+          child: Text('Log In'),
+        ),
+        onPressed: () {},
       ),
     );
   }
@@ -28,7 +145,7 @@ class LogInPage extends StatelessWidget {
 
     final TextStyle style = TextStyle(
       color: Colors.white,
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeight.w300,
       fontSize: 30,
     );
 
@@ -56,7 +173,7 @@ class LogInPage extends StatelessWidget {
 
     final person = Center(
       child: Container(
-        padding: EdgeInsets.only(top: 80.0),
+        padding: EdgeInsets.only(top: 60.0),
         child: Column(
           children: [
             Icon(
