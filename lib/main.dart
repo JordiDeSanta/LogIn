b0ccb8528fff9efda437521f2ba110fabbca2c77
Jordilob <jordi.bravo.bad@gmail.com>
2021-04-06 18:04:09 -0400

    Implementing in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login/src/bloc/provider.dart';
 
 import 'package:login/src/pages/home_page.dart';
 import 'package:login/src/pages/login_page.dart';
@@ -8,14 +9,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Log In App',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LogInPage(),
-        'home': (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Log In App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LogInPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+      ),
     );
   }
 }
