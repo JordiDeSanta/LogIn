1806ec09b79c90835d8875ca392b09db347f2c2f
Jordilob <jordi.bravo.bad@gmail.com>
2021-04-08 22:36:03 -0400

    Combining streams with Rx

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'package:login/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginBloc with Validators {
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   Stream<String> get emailStream =>
       _emailController.stream.transform(validateEmail);
   Stream<String> get passwordStream =>
       _passwordController.stream.transform(validatePassword);
+  Stream<bool> get formValidStream =>
+      Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
