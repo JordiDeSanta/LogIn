74fe33d2ccd806a435c3c471cc6ff677268e7ea6
Jordilob <jordi.bravo.bad@gmail.com>
2021-04-08 23:15:29 -0400

    Finishing with Bloc fixing

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -3,9 +3,19 @@ import 'package:login/src/bloc/login_bloc.dart';
 export 'package:login/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
-  final loginBloc = LoginBloc();
+  static Provider _instance;
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  factory Provider({Key key, Widget child}) {
+    if (_instance == null) {
+      _instance = new Provider._internal(key: key, child: child);
+    }
+
+    return _instance;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
+  final loginBloc = LoginBloc();
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
