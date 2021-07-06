8d17ba525c7a2ff1c0a84992810a33965122865c
Jordi <jordi.bravo.bad@gmail.com>
2021-07-06 19:14:58 -0400

    Adding Products Bloc in Provider

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+
 import 'package:login/src/bloc/login_bloc.dart';
 export 'package:login/src/bloc/login_bloc.dart';
 
+import 'package:login/src/bloc/products_bloc.dart';
+export 'package:login/src/bloc/products_bloc.dart';
+
 class Provider extends InheritedWidget {
+  final _loginBloc = new LoginBloc();
+  final _productsBloc = new ProductsBloc();
+
   static Provider _instance;
 
   factory Provider({Key key, Widget child}) {
@@ -15,12 +22,14 @@ class Provider extends InheritedWidget {
 
   Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
-  final loginBloc = LoginBloc();
-
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 
   static LoginBloc of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+    return context.dependOnInheritedWidgetOfExactType<Provider>()._loginBloc;
+  }
+
+  static ProductsBloc productsBloc(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>()._productsBloc;
   }
 }
